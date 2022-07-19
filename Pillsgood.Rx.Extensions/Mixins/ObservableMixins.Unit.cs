368210f65735c81bbc752573158779daca0dc0bf
Pillsgood <arashm35@gmail.com>
2022-07-19 22:01:23 +0800

    add AsUnit extension method;

---
+++ Pillsgood.Rx.Extensions/Mixins/ObservableMixins.Unit.cs
@@ -0,0 +1,13 @@
+﻿using System.Reactive;
+using System.Reactive.Linq;
+
+// ReSharper disable once CheckNamespace
+namespace System;
+
+public static partial class ObservableMixins
+{
+    public static IObservable<Unit> AsUnit<T>(this IObservable<T> source)
+    {
+        return source.Select(_ => Unit.Default);
+    }
+}
